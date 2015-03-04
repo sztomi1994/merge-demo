a35f90e6df907e36346b89f20d4124542ed3a73f
unknown <poker07@A109-03.itlab.pmmk.pte.hu>
2015-03-04 17:59:06 +0100

    ez biza merge lesz

---
+++ merge/src/main/java/com/mycompany/merge/Cucc.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.mycompany.merge;
+
+/**
+ *
+ * @author poker07
+ */
+public class Cucc {
+    
+}
